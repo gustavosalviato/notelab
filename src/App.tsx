5236ef6f9111b1d0ed3f76733798c8dff33451bd
gustavosalviato <gustavosalviato8@gmail.com>
2023-10-05 20:51:43 -0300

    chore: page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react'
 import { Editor } from './component/Editor'
 
 export function App() {
+  useEffect(() => {
+    document.title = 'notelab | home'
+  }, [])
+
   return (
     <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
       <div className="bg-zinc-800 mx-auto rounded-xl min-h-[45rem] border border-black/20 shadow-sm overflow-hidden max-w-5xl">
